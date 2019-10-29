a24bd7d8c4f50c7122fffd34eb381ecb4c071552
adrianomerodack <adrianocunha@bemol.com.br>
2019-10-29 00:22:31 -0400

    Add access in python admin for teams app

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Teams
 
-# Register your models here.
+@admin.register(Teams)
+class TeamsAdmin(admin.ModelAdmin):
+    list_display = ['s_name', 'i_indicador']
