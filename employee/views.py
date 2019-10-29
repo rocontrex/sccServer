e2c2dbeb886626f91f91fdfe8783c20643ea811a
Rodrigo Conte <rodrigo.conte14@gmail.com>
2019-10-29 11:33:34 -0400

    Refactoring employee Model

--- employee/views.py
+++ employee/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render, redirect
 from .models import Employee
+from .forms import EmployeeForm
 
 
-def listar_employee(request):
+def list_employee(request):
     employee = Employee.objects.all()
     return render(request, '', {'employee': employee})
 
-def criar_employee(request):
-    return
+def create_employee(request):
+    form = EmployeeForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('list_employee')
+    
+    return render(request, '', {'form': form})
 
-def atualizar_employee(request):
-    return
+def update_employee(request):
+    employee = Employee.objects.get(id=id)
+    form = EmployeeForm(request.POST or None, instance=employee)
 
-def deletar_employee(request):
-    return
+    if form.is_valid():
+        form.save()
+        return redirect(request, '', {'form': form})
+
+    return render(request, '', {'form': form, 'employee': employee})
+
+def delete_employee(request):
+    employee = Employee.objects.get(id=id)
+
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('list_employee')
+
+    return render(request, '', {'employee':employee})
